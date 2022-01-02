8cddbd01975f813f12f35dc34dc0297729efd7c1
Jeyalakshmisureshpaul <94035689+Jeyalakshmisureshpaul@users.noreply.github.com>
2022-01-02 23:51:59 +0530

    Update pagination.js

--- pagination.js
+++ pagination.js
@@ -12,13 +12,12 @@ var currentPage = document.getElementById('currentPage')
 var totalPage = document.getElementById('totalPages')
 var pageCon = document.getElementById('pageBtns')
 
-// initiate class for pagination task
+
 
 class Pagination {
 constructor(){
     this.firstIndex = 0 
     
-// generate buttons
 data.then(data=>{
 var numOfBtn = data.length/10;
 for(let i = 0 ; i < numOfBtn ; i++){
@@ -31,17 +30,16 @@ for(let i = 0 ; i < numOfBtn ; i++){
 }).catch(err => console.log(err))
 }
 
-// logic for pagination buttons
+
 buttons(){
     data.then(data=>{
         
-        // condition logic for next button
         if(this.firstIndex < data.length-10  && this.firstIndex >= 0){
         nextBtn.style.display = "block"
     } else {
         nextBtn.style.display = "none"
     }
-    // condition logic for prev btn
+    
 
     if(this.firstIndex > 0 && this.firstIndex < data.length){
         prevBtn.style.display = "block"
@@ -52,20 +50,13 @@ buttons(){
     }).catch(err => console.log(err))
 }
 
-// to display table contents
 display(){
 data.then(data=>{
-    // console.log(data.length)
- 
-    
-    //totalPage.innerHTML = data.length/10;
     currentPage.innerHTML = (this.firstIndex/10)+1
     
-    //display table
     tbody.innerHTML = ''
     for(let i = this.firstIndex ; i < this.firstIndex+10; i++){
     
-        // console.log(data[i].id)
         var row = tr()
         var rowData = [td(), td(), td()]
         rowData[0].innerHTML = data[i].id
@@ -79,7 +70,6 @@ data.then(data=>{
 this.buttons()
 }
 
-// to change next page
 next(){
    
     this.firstIndex = this.firstIndex + 10
@@ -87,13 +77,11 @@ next(){
    
 }
 
-// to change previous page
 prev(){
     this.firstIndex = this.firstIndex - 10
     this.display()
 }
 
-// to set the page number
 setPage(num){
 this.firstIndex = num * 10
 this.display()
@@ -107,8 +95,6 @@ lastPage(){
 
 var page = new Pagination()
 page.display()
-
-// button actions i.e click event actions
 
 firstBtn.addEventListener('click', ()=>page.setPage(0))
 lastBtn.addEventListener('click', ()=>page.lastPage())
